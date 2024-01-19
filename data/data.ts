770fc9434cf995c0c8488ac8bd2cb965e4a33aec
RSDev1314 <rhonmark.dev@gmail.com>
2024-01-18 23:07:18 -0700

    Commit

---
+++ data/data.ts
@@ -0,0 +1,97 @@
+export const employeeData = [
+  {
+    id: 1,
+    name: "Rhonmark S.",
+    position: "Verified Account",
+    transactions: 218938,
+    rise: true,
+    tasksCompleted: 5,
+    imgId: 0,
+  },
+
+];
+
+export const Countrydata = [
+  { name: "USD/JPY", rise: true, value: 148.45, id: 1 },
+  { name: "EUR/USD", rise: false, value: 1.0885, id: 2 },
+  { name: "GBP/USD", rise: false, value: 1.2754, id: 3 },
+  { name: "ETH/USDT", rise: true, value: 2467.01, id: 4 },
+];
+export const segmentationData = [
+  { c1: "USD/JPY", c2: "1054", c3: "#363636", color: "#535353" },
+  { c1: "EUR/USD", c2: "341", c3: "#818bb1", color: "#595f77" },
+  { c1: "GBP/USD", c2: "133", c3: "#2c365d", color: "#232942" },
+  { c1: "ETH/USDT", c2: "26", c3: "#334ed8", color: "#2c3051" },
+
+];
+
+export const sidebarItems = [
+  [
+    { id: "0", title: "Dashboard", notifications: false },
+    { id: "1", title: "Overview", notifications: false },
+    { id: "2", title: "Chat", notifications: 6 },
+    { id: "3", title: "Team", notifications: false },
+  ],
+  [
+    { id: "4", title: "Tasks", notifications: false },
+    { id: "5", title: "Reports", notifications: false },
+    { id: "6", title: "Settings", notifications: false },
+  ],
+];
+
+export const graphData = [
+  {
+    name: "Nov",
+    revenue: 55938,
+    expectedRevenue: 2467,
+    sales: 203,
+  },
+  {
+    name: "Dec",
+    revenue: 108938,
+    expectedRevenue: 2584,
+    sales: 458,
+  },
+  {
+    name: "Jan",
+    revenue: 76096,
+    expectedRevenue: 2980,
+    sales: 264,
+  },
+  {
+    name: "Feb",
+    revenue: 110764,
+    expectedRevenue: 3587,
+    sales: 415,
+  },
+  {
+    name: "Mar",
+    revenue: 137056,
+    expectedRevenue: 3875,
+    sales: 317,
+  },
+  {
+    name: "Apr",
+    revenue: 123476,
+    expectedRevenue: 3867,
+    sales: 221,
+  },
+  {
+    name: "May",
+    revenue: 159864,
+    expectedRevenue: 4683,
+    sales: 282,
+  },
+  {
+    name: "June",
+    revenue: 180873,
+    expectedRevenue: 5743,
+    sales: 31,
+  },
+  {
+    name: "July",
+    revenue: 218938,
+    expectedRevenue: 5086,
+    sales: 245,
+  },
+];
